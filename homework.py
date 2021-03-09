8da9b816675242c795a08f13d08179a2c3555398
Сергей <7atom7@mail.ru>
2021-03-09 21:29:22 +0300

    Third

--- homework.py
+++ homework.py
@@ -19,7 +19,7 @@ TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 YA_API_URL = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
 
-STATUSES = {
+VERDICT = {
     'rejected': 'К сожалению в работе нашлись ошибки.',
     'approved': 'Ревьюеру всё понравилось,'
                 ' можно приступать к следующему уроку.',
@@ -34,12 +34,13 @@ def parse_homework_status(homework):
     if homework_name is None or homework_status is None:
         logging.error('Проверьте загружена ли домашка')
         return f'Ошибка, {homework_name} не работает правильно или отсутствует'
-    for status, verdict in STATUSES.items():
-        if homework_status == status:
-            return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
-        elif verdict is None:
-            logging.warning(f'Неизвестный статус домашки: {homework_status}')
-            return f'Ошибка: {homework_status} не сопоставим с {verdict}'
+    elif homework_status in VERDICT.keys():
+        return 'У вас проверили работу ' \
+               f'"{homework_name}"!\n\n{VERDICT[homework_status]}'
+    else:
+        logging.warning(f'Неизвестный статус домашки: {homework_status}')
+        return 'Ошибка:' \
+               f'{homework_status} не сопоставим с {VERDICT[homework_status]}'
 
 
 def get_homework_statuses(current_timestamp):
