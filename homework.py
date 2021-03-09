e0698d64042248dd43c7b83841d59cd9c258261a
Сергей <7atom7@mail.ru>
2021-03-09 20:45:23 +0300

    Third

--- homework.py
+++ homework.py
@@ -19,26 +19,27 @@ TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 YA_API_URL = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
 
+STATUSES = {
+        'rejected': 'К сожалению в работе нашлись ошибки.',
+        'approved': 'Ревьюеру всё понравилось,'
+                     ' можно приступать к следующему уроку.',
+        'reviewing': 'Работа в процессе проверки',
+}
 
 def parse_homework_status(homework):
     logging.info('Определение этапа проверки')
     homework_name = homework.get('homework_name')
     homework_status = homework.get('status')
-    statuses = [
-        ['rejected', 'К сожалению в работе нашлись ошибки.'],
-        ['approved', 'Ревьюеру всё понравилось,'
-                     ' можно приступать к следующему уроку.'],
-        ['reviewing', 'Работа в процессе проверки'],
-    ]
+    homework_verdict = homework.get('verdict')
     if homework_name is None or homework_status is None:
         logging.error('Проверьте загружена ли домашка')
         return f'Ошибка, {homework_name} не работает правильно или отсутствует'
-    for status, verdict in statuses:
+    for status, verdict in STATUSES.items():
         if homework_status == status:
             return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
         elif verdict is None:
-            logging.warning('Неизвестный статус домашки!')
-            return f'Ошибка статуса: {status} не сопоставим с {verdict}'
+            logging.warning(f'Неизвестный статус домашки: {homework_status}')
+            return f'Ошибка: {homework_status} не сопоставим с {verdict}'
 
 
 def get_homework_statuses(current_timestamp):
@@ -48,11 +49,10 @@ def get_homework_statuses(current_timestamp):
     params = {'from_date': current_timestamp}
     try:
         homework_statuses = requests.get(url, params=params, headers=headers)
-        return homework_statuses.json()
-    except Exception as e:
-        logging.error(f'Ошибка получения статуса домашки: {e}')
-        time.sleep(5)
-        pass
+    except requests.exceptions.RequestException as e:
+        logging.error('Ошибка получения статуса домашки'
+                      f'с заголовком {headers} и параметрами {params} : {e}')
+    return homework_statuses.json()
 
 
 def send_message(message, bot_client):
@@ -68,15 +68,17 @@ def main():
             new_homework = get_homework_statuses(current_timestamp)
             if new_homework.get('homeworks'):
                 send_message(
-                    parse_homework_status(new_homework[0]), bot
+                    parse_homework_status(new_homework['homeworks'][0]), bot
                 )
             current_timestamp = new_homework.get('current_date',
                                                  current_timestamp)
             time.sleep(300)
 
-        except Exception as e:
-            logging.error(f'Бот столкнулся с ошибкой: {e}')
-            time.sleep(5)
+        except requests.exceptions.RequestException as e:
+            logging.error(f'Бот столкнулся с ошибкой запроса: {e}')
+            send_message(
+                f'Бот столкнулся с ошибкой запроса: {e}', bot
+                )
 
 
 if __name__ == '__main__':
